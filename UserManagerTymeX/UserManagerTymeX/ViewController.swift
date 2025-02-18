934976be6890fc2a76a72b5e1ab25bba7379d675
nguyen minh tuan <tuan88.it@gmail.com>
2025-02-18 19:47:25 +0700

    Initial Project
    First Initial Project
    - create Project using UIKit

---
+++ UserManagerTymeX/UserManagerTymeX/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UserManagerTymeX
+//
+//  Created by nguyen minh tuan on 2/18/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
