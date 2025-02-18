ecccf8c3fb837c37c05b5d866b572aa568400882
nguyen minh tuan <tuan88.it@gmail.com>
2025-02-18 22:32:20 +0700

    Implement Domain Layer
    - add FetchUsersUseCase
    - add FetchUserDetailUseCase

--- UserManagerTymeX/Package/Domain/Package.swift
+++ UserManagerTymeX/Package/Domain/Package.swift
@@ -5,20 +5,35 @@ import PackageDescription
 
 let package = Package(
     name: "Domain",
+    platforms: [
+        .iOS(.v15)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "Domain",
             targets: ["Domain"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "6.8.0")
+    ],
+    
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Domain"),
+            name: "Domain",
+            dependencies: [
+                "RxSwift"
+            ]
+        ),
         .testTarget(
             name: "DomainTests",
-            dependencies: ["Domain"]
+            dependencies: [
+                "Domain",
+                .product(name: "RxTest", package: "RxSwift"),
+                .product(name: "RxBlocking", package: "RxSwift")
+            ]
         ),
     ]
 )
---
+++ UserManagerTymeX/Package/Domain/Sources/Domain/Entries/User.swift
@@ -0,0 +1,22 @@
+//
+//  User.swift
+//  Domain
+//
+//  Created by nguyen minh tuan on 2/18/25.
+//
+
+import Foundation
+
+public struct User: Equatable {
+    public let userId: Int
+    public let login: String
+    public let avatarUrl: String
+    public let htmlUrl: String
+    
+    public init(userId: Int, login: String, avatarUrl: String, htmlUrl: String) {
+        self.userId = userId
+        self.login = login
+        self.avatarUrl = avatarUrl
+        self.htmlUrl = htmlUrl
+    }
+}
